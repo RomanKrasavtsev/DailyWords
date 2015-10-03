e63d63b38b6ff3e97eb14b24787d952898912b19
Roman Krasavtsev <mr.krasavtsev@gmail.com>
2015-10-03 16:02:06 +0300

    Refactoring

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-require 'nokogiri'
-require 'open-uri'
+require "nokogiri"
+require "open-uri"
 
 wiki_doc = Nokogiri::HTML(
-  open('https://en.wikipedia.org/wiki/Most_common_words_in_English')
+  open("https://en.wikipedia.org/wiki/Most_common_words_in_English")
 )
 
 wiki_words = []
-wiki_doc.css('.column ol li').each do |word|
+wiki_doc.css(".column ol li").each do |word|
   wiki_words << word.content.downcase
 end
 
@@ -24,7 +24,7 @@ wiki_words.uniq.sort.each do |original_text|
   )
 
   transcription = "[#{cambridge_doc.css('.ipa')[0].text}]"
-  translated_text = cambridge_doc.css('.trans')[0].text.strip!
+  translated_text = cambridge_doc.css(".trans")[0].text.strip!
 
   words << [original_text, transcription, translated_text]
 end

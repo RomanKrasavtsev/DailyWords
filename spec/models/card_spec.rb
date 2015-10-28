75b86d67ed4a9a75571a0ce314f99cfbfad37803
Roman Krasavtsev <mr.krasavtsev@gmail.com>
2015-10-28 10:08:53 +0300

    refactoring

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,13 +1,17 @@
 require "rails_helper"
 
 RSpec.describe Card, :type => :model do
-  card = Card.new(original_text: "house", translated_text: "дом", transcription: "[haʊs]")
-  
-  it 'will return false' do
+  card = Card.new(
+    original_text: "house",
+    translated_text: "дом",
+    transcription: "[haʊs]"
+  )
+
+  it "will return false" do
     expect(card.check_translation("house")).to be true
   end
 
-  it 'will return true' do
+  it "will return true" do
     expect(card.check_translation("Дом")).to be false
   end
 end

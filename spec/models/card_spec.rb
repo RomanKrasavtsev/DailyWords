67b91e7e25b41d5987ab1bdbac861beb21c806f0
Roman Krasavtsev <mr.krasavtsev@gmail.com>
2015-10-29 23:06:32 +0300

    refactoring

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe Card, type: :model do
+describe Card do
   it "validates with erorrs" do
     card = Card.new(
       original_text: "Дом",
@@ -12,24 +12,26 @@ RSpec.describe Card, type: :model do
     expect(card.errors).to be_present
   end
 
-  let(:card) do
-    Card.new(
-      original_text: "house",
-      translated_text: "дом",
-      transcription: "[haʊs]"
-    )
-  end
+  context "when checked translation" do
+    let(:card) do
+      Card.new(
+        original_text: "house",
+        translated_text: "дом",
+        transcription: "[haʊs]"
+      )
+    end
 
-  it "validates without errors" do
-    card.valid?
-    expect(card.errors).to be_blank
-  end
+    it "validates without errors" do
+      card.valid?
+      expect(card.errors).to be_blank
+    end
 
-  it "checks translation and will return true" do
-    expect(card.check_translation("house")).to be true
-  end
+    it "will return true" do
+      expect(card.check_translation("house")).to be true
+    end
 
-  it "checks translation and will return false" do
-    expect(card.check_translation("Дом")).to be false
+    it "will return false" do
+      expect(card.check_translation("Дом")).to be false
+    end
   end
 end

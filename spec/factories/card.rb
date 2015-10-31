6f8d823a36882c2f08d9c64b80b95288768317ce
Roman Krasavtsev <mr.krasavtsev@gmail.com>
2015-11-01 00:53:50 +0300

    seventh-task

---
+++ spec/factories/card.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+
+  factory :card do
+    original_text "house"
+    translated_text "дом"
+    transcription "[haʊs]"
+
+    trait :expired do
+      review_date -3.day.since
+    end
+  end
+
+end
---
+++ spec/features/review_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+feature "review" do
+  context "can start" do
+
+    before(:each) do
+      card = create(:card, :expired)
+    end
+
+    it  "can check card" do
+      visit root_path
+      fill_in "review_entered_text", :with => "House"
+      click_button "Проверить"
+      expect(page).to have_content "Верно!"
+    end
+
+  end
+end

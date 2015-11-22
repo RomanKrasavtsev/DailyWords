cb93802c2f898bf43cb8ea922bee467eb11be81e
Roman Krasavtsev <mr.krasavtsev@gmail.com>
2015-11-22 20:26:25 +0300

    refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'password_resets/create'
-  get 'password_resets/edit'
-  get 'password_resets/update'
+  get "password_resets/create"
+  get "password_resets/edit"
+  get "password_resets/update"
   root "reviews#new"
 
   get "signup"    => "users#new",         as: "signup"

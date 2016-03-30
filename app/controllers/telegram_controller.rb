ed713c78c136bc9bbab847d000ae04b42dd987d4
Roman Krasavtsev <mr.krasavtsev@gmail.com>
2016-03-30 20:24:16 +0300

    Add Telegram

--- app/controllers/telegram_controller.rb
+++ app/controllers/telegram_controller.rb
@@ -13,18 +13,14 @@ class TelegramController < ApplicationController
     if text == "ok"
       chat_id = params["message"]["chat"]["id"]
 
-      answer = prepare_message("Привет! Ваш ID: #{chat_id}")
-        logger.debug "###### #{answer}"
+      answer = "Привет! Ваш ID: #{chat_id}".gsub(" ","%20").gsub("+","%2B")
+      logger.debug "+++ #{answer} +++"
       JSON.load(open("https://api.telegram.org/bot#{token}/sendMessage?chat_id=#{chat_id}&text=#{answer}"))
     end
 
   end
 
   private
-
-  def prepare_message(text)
-    text.gsub!(" ","%20").gsub!("+","%2B")
-  end
 
   def telegram_params
     # params.permit(
